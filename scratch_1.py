486797ca695a5394042164534ea90a2f773b570a
anomaly3108 <46282160+avirlshrma@users.noreply.github.com>
2019-10-12 16:13:50 +0530

    Add files via upload

---
+++ scratch_1.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get("https://www.youtube.com/feed/trending")
+soup = BeautifulSoup(source.text,'lxml')
+
+csv_file = open('YouTube Trending Titles on 12-30-18.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Title', 'Description'])
+
+for content in soup.find_all('div', class_= "yt-lockup-content"):
+    try:
+        title = content.h3.a.text
+        print(title)
+
+        description = content.find('div', class_="yt-lockup-description yt-ui-ellipsis yt-ui-ellipsis-2").text
+        print(description)
+
+    except Exception as e:
+        description = None
+
+    print('\n')
+    csv_writer.writerow([title, description])
+
+csv_file.close()
